02095bee59f92589655ff264869680d4330ca232
HaskellHydra <hackinghaskell@gmail.com>
2022-05-28 23:28:27 -0700

    Setting up Trace

--- src/Main.hs
+++ src/Main.hs
@@ -13,16 +13,19 @@
 {-# LANGUAGE ImportQualifiedPost  #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving  #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
-
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE LambdaCase#-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 module Main where
 
+import qualified Data.Aeson as Aeson
 import Control.Monad (void)
 import Data.ByteString.Char8 qualified as C
--- import Data.Map (Map)
--- import Data.Map qualified as Map
+import Data.Map (Map)
+import Data.Map qualified as Map
 import Data.Maybe (catMaybes)
+import Data.Default               (def, Default (..))
 import Ledger (Address, dataHash ,Datum (Datum), DatumHash (..), ScriptContext, Validator, Value, getCardanoTxId)
 import Ledger 
 import Ledger.Ada qualified as Ada
@@ -38,6 +41,15 @@ import           Text.Printf          (printf)
 -- import PlutusTx.Builtins
 import PlutusTx.AssocMap qualified as AssocMap
 
+-- Trace imports
+import Plutus.Trace as Trace
+import Wallet.Emulator.Wallet
+import qualified Control.Monad.Freer.Extras as Extras
+import Wallet.Emulator.MultiAgent
+import Plutus.Trace.Emulator.Types
+import System.IO
+
+
 type AMap = AssocMap.Map 
 
 data Game = Game
@@ -48,7 +60,16 @@ data Game = Game
     , identifierToken     :: !AssetClass
     } deriving (Show, Generic, FromJSON, ToJSON, Haskell.Eq, Haskell.Ord)
 
-PlutusTx.makeLift ''Game
+instance Eq Game where
+    {-# INLINABLE (==) #-}
+    a == b =
+        (tFirstPlayer a == tFirstPlayer b) &&
+        (tSecondPlayer a == tSecondPlayer b) &&
+        (tGameStake a == tGameStake b) &&
+        (tDeadline a == tDeadline b) &&
+        (identifierToken a == identifierToken b)
+
+PlutusTx.unstableMakeIsData ''Game
 
 data GameChoice = X | O | N
     deriving (Show, Generic, FromJSON, ToJSON, ToSchema, Haskell.Eq, Haskell.Ord)
@@ -62,12 +83,20 @@ instance Eq GameChoice where
 
 PlutusTx.unstableMakeIsData ''GameChoice
 
-data GameDatum = GameDatum (AMap BuiltinByteString GameChoice) | NoDatum
+data GameState = GameState (AMap BuiltinByteString GameChoice)
     deriving (Show, Haskell.Eq)
 
-instance Eq GameDatum where
+instance Eq GameState where
     {-# INLINABLE (==) #-}
-    GameDatum cs == GameDatum cs' = (cs == cs')
+    GameState cs == GameState cs' = (cs == cs')
+
+PlutusTx.unstableMakeIsData ''GameState
+
+data GameDatum = GameDatum {
+    gdGame :: !Game
+   ,gdGameState :: !GameState    
+  }
+  deriving (Show)
 
 PlutusTx.unstableMakeIsData ''GameDatum
 
@@ -76,17 +105,124 @@ data GameRedeemer = Play GameChoice | ClaimFirst | ClaimSecond
 
 PlutusTx.unstableMakeIsData ''GameRedeemer
 
-{-# INLINABLE initDatum #-}
-initDatum :: GameDatum
-initDatum = GameDatum (AssocMap.fromList t)
+{-# INLINABLE initGameState #-}
+initGameState :: GameState
+initGameState = GameState (AssocMap.fromList t)
               where 
                   y = (\x -> (("p" Haskell.<> Haskell.show x) <> ) Haskell.<$> ((Haskell.show) Haskell.<$> [1..3::Integer] )) Haskell.<$> [1..3::Integer]
                   t = zip ((toBuiltin . C.pack) Haskell.<$> (concat y)) (Haskell.take 9 $ Haskell.repeat N)
 
-{-# INLINABLE getDatumMap #-}
-getDatumMap :: GameDatum -> (AMap BuiltinByteString GameChoice)
-getDatumMap (GameDatum m) = m 
+{-# INLINABLE getGameStateMap #-}
+getGameStateMap :: GameState -> (AMap BuiltinByteString GameChoice)
+getGameStateMap (GameState m) = m 
 
 {-# INLINABLE mkValidator #-}
-mkValidator :: Game -> GameDatum -> GameRedeemer -> ScriptContext -> Bool
-mkValidator game datum redeemer ctx = Haskell.undefined
+mkValidator :: GameDatum -> GameRedeemer -> ScriptContext -> Bool
+mkValidator datum redeemer ctx = (traceIfFalse "Wrong datum" checkCorrectDatum ) &&
+                                 (traceIfFalse "Deadline expired! (or) Unable to extract datum" checkDeadline)                                 
+    where
+        checkDeadline :: Bool
+        checkDeadline = contains (from $ tDeadline $ gdGame datum) $ txInfoValidRange $ scriptContextTxInfo ctx
+
+        checkCorrectDatum :: Bool
+        checkCorrectDatum = True
+
+    ------------------------------
+    -- TODO: In the below code check if the `findDatum h info` is extracting the datum from the output.
+    ------------------------------
+
+    -- ownOutput   :: TxOut
+    -- outputDatum :: AuctionDatum
+    -- (ownOutput, outputDatum) = case getContinuingOutputs ctx of
+    --     [o] -> case txOutDatumHash o of
+    --         Nothing   -> traceError "wrong output type"
+    --         Just h -> case findDatum h info of
+    --             Nothing        -> traceError "datum not found"
+    --             Just (Datum d) ->  case PlutusTx.fromBuiltinData d of
+    --                 Just ad' -> (o, ad')
+    --                 Nothing  -> traceError "error decoding data"
+    --     _   -> traceError "expected exactly one continuing output"
+
+-- | Datum and redeemer parameter types
+data TicTacToe
+instance Scripts.ValidatorTypes TicTacToe where
+    type instance RedeemerType TicTacToe = GameRedeemer
+    type instance DatumType TicTacToe = GameDatum
+
+ticTacToeInstance ::Scripts.TypedValidator TicTacToe
+ticTacToeInstance = Scripts.mkTypedValidator @TicTacToe
+  $$(PlutusTx.compile [|| mkValidator ||])
+  $$(PlutusTx.compile [|| wrap ||])
+    where
+        wrap = Scripts.wrapValidator @GameDatum @GameRedeemer
+
+------------------------------------------------------------
+-- | Off-Chain code
+------------------------------------------------------------
+
+validator :: Validator
+validator = Scripts.validatorScript ticTacToeInstance
+
+-- | The address of the bounty (the hash of its validator script)
+gameAddress :: Address
+gameAddress = Ledger.scriptAddress (Scripts.validatorScript ticTacToeInstance)
+
+
+
+-- | The schema of the contract, with one endpoint to publish the problem with a bounty and another to sbumit solutions
+type TicTacToeSchema = Endpoint "test" ()
+
+
+test :: forall w s e. AsContractError e => Contract w s e ()
+test = do
+    logInfo @Haskell.String "-----------Printing test logs---------"
+    unspentOutputs <- utxosAt gameAddress
+    -- 'collectFromScript' is a function of the wallet API. It creates a tx consuming
+    -- all unspent transaction outputs at a script address and pays them to a
+    -- public key address owned by this wallet.
+    logInfo @Haskell.String $ Haskell.show $ Map.toList unspentOutputs
+
+
+-- | Math bounty endpoints.
+endpoints :: forall  e. AsContractError e => Contract () TicTacToeSchema e ()
+endpoints = awaitPromise (test') >> endpoints
+  where
+    test' = endpoint @"test" $ const test
+
+mkSchemaDefinitions ''TicTacToeSchema
+
+
+emulatorConfig :: Trace.EmulatorConfig
+emulatorConfig =
+  Trace.EmulatorConfig
+    (Left $ Map.fromList [(knownWallet 1, Ada.lovelaceValueOf 30_000_000),
+                          (knownWallet 2, Ada.lovelaceValueOf 10_000_000)])
+    def
+    def
+
+customShowEvent :: EmulatorEvent' -> Maybe Haskell.String
+customShowEvent = \case
+  UserThreadEvent (UserLog msg)                                        -> Just $ "*** USER LOG: " <> msg
+  InstanceEvent (ContractInstanceLog (ContractLog (Aeson.String msg)) _ _)   -> Just $ "*** CONTRACT LOG: " <> Haskell.show msg
+  InstanceEvent (ContractInstanceLog (StoppedWithError err)       _ _) -> Just $ "*** CONTRACT STOPPED WITH ERROR: " <> Haskell.show err
+  ev                                                                   -> Nothing
+
+traceConfig :: Trace.TraceConfig
+traceConfig =
+  Trace.TraceConfig
+    customShowEvent
+    stdout
+
+-- test consuming the bounty when the deadline has not been reached
+myTraceTest :: Trace.EmulatorTrace ()
+myTraceTest = do
+    h1 <- Trace.activateContractWallet (knownWallet 1) $ endpoints @ContractError
+    h2 <- Trace.activateContractWallet (knownWallet 2) $ endpoints @ContractError
+    Trace.callEndpoint @"test" h1 ()
+    s <- Trace.waitNSlots 1
+    Extras.logInfo $ "reached " ++ Haskell.show s
+
+
+test1 :: IO ()
+-- test1 = Trace.runEmulatorTraceIO' traceConfig emulatorConfig myTraceTest
+test1 = Trace.runEmulatorTraceIO' def def myTraceTest
