53842a5bad5009606e17c2fa7c85e01034ce7927
HaskellHydra <hackinghaskell@gmail.com>
2022-06-02 07:35:31 -0700

    Adding make a move endpoint and validation logic. Several checks are still missing

--- src/Main.hs
+++ src/Main.hs
@@ -27,6 +27,7 @@ import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (catMaybes)
 import Data.Default               (def, Default (..))
+import           Data.Text            (pack, Text)
 import Ledger (Address, dataHash ,Datum (Datum), DatumHash (..), ScriptContext, Validator, Value, getCardanoTxId)
 import Ledger 
 import Ledger.Value as Value
@@ -46,7 +47,7 @@ import Ledger.TimeSlot
 import PlutusTx.AssocMap qualified as AssocMap
 
 -- Trace imports
-import Plutus.Trace as Trace
+import qualified Plutus.Trace as Trace
 import Wallet.Emulator.Wallet
 import qualified Control.Monad.Freer.Extras as Extras
 import Wallet.Emulator.MultiAgent
@@ -61,10 +62,15 @@ type AMap = AssocMap.Map
 minLovelace :: Integer
 minLovelace = 2_000_000
 
+minGameStake :: Integer
+minGameStake = 10_000_000
+
+
 data Game = Game
     { tFirstPlayer        :: !(BuiltinByteString, PaymentPubKeyHash)
     , tSecondPlayer       :: !(Maybe (BuiltinByteString, PaymentPubKeyHash))
     , tGameStake          :: !Integer
+    , tMinGameStake       :: !Integer
     , tDeadline           :: !POSIXTime
     , identifierToken     :: !AssetClass
     } deriving (Show, Generic, FromJSON, ToJSON, Haskell.Eq, Haskell.Ord)
@@ -110,9 +116,13 @@ data GameDatum = GameDatum {
 PlutusTx.unstableMakeIsData ''GameDatum
 
 data GameRedeemer = Play GameChoice | ClaimFirst | ClaimSecond
-    deriving Show
+    deriving (Show, Generic, ToJSON, FromJSON)
 
 PlutusTx.unstableMakeIsData ''GameRedeemer
+
+{-# INLINABLE lovelaces #-}
+lovelaces :: Value -> Integer
+lovelaces = Ada.getLovelace . Ada.fromValue
 
 {-# INLINABLE initGameState #-}
 initGameState :: GameState
@@ -125,32 +135,86 @@ initGameState = GameState (AssocMap.fromList t)
 getGameStateMap :: GameState -> (AMap BuiltinByteString GameChoice)
 getGameStateMap (GameState m) = m 
 
+
 {-# INLINABLE mkValidator #-}
 mkValidator :: GameDatum -> GameRedeemer -> ScriptContext -> Bool
-mkValidator datum redeemer ctx = (traceIfFalse "Wrong datum" checkCorrectDatum ) &&
+mkValidator datum redeemer ctx = (traceIfFalse "Wrong datum" checkPlayerChoice ) &&
                                  (traceIfFalse "Deadline expired! (or) Unable to extract datum" checkDeadline)                                 
     where
         checkDeadline :: Bool
-        checkDeadline = contains (from $ tDeadline $ gdGame datum) $ txInfoValidRange $ scriptContextTxInfo ctx
+        checkDeadline = contains (to $ tDeadline $ gdGame datum) (txInfoValidRange $ scriptContextTxInfo ctx)
 
-        checkCorrectDatum :: Bool
-        checkCorrectDatum = True
+        -- If the second player is nothing in the output datum and the if the input is 'Just x' it is false
+        -- If the second player is nothing both in input and output datums then player 2 did not join the game
+          -- The game stake is only 1* gamestake      
 
-    ------------------------------
-    -- TODO: In the below code check if the `findDatum h info` is extracting the datum from the output.
-    ------------------------------
+        -- evalGameTx :: Bool
+        -- evalGameTx = let inGameCfg = gdGame GameDatum
+        --                  outGameCfg = gdGame outputDatum in
+        --                    case (datum , redeemer) of
 
-    -- ownOutput   :: TxOut
-    -- outputDatum :: AuctionDatum
-    -- (ownOutput, outputDatum) = case getContinuingOutputs ctx of
-    --     [o] -> case txOutDatumHash o of
-    --         Nothing   -> traceError "wrong output type"
-    --         Just h -> case findDatum h info of
-    --             Nothing        -> traceError "datum not found"
-    --             Just (Datum d) ->  case PlutusTx.fromBuiltinData d of
-    --                 Just ad' -> (o, ad')
-    --                 Nothing  -> traceError "error decoding data"
-    --     _   -> traceError "expected exactly one continuing output"
+        inGameCfg :: Game
+        inGameCfg = gdGame datum
+
+        outGameCfg :: Game
+        outGameCfg = gdGame outputDatum
+
+        checkPlayerChoice :: Bool
+        checkPlayerChoice = case redeemer of
+                              Play c -> if (c == X) then
+                                          (txSignedBy info $ unPaymentPubKeyHash $ snd $ tFirstPlayer inGameCfg )
+                                        else if ( c == O ) then
+                                          case (tSecondPlayer inGameCfg) of
+                                            Nothing -> False 
+                                            Just x -> (txSignedBy info $ unPaymentPubKeyHash $ snd x )
+                                        else
+                                            traceError "Wrong game choice"
+                              ClaimFirst -> True  -- TODO
+                              ClaimSecond -> True -- TODO  
+
+
+        -- checkCorrectDatum = 
+        --   let inGameCfg = gdGame GameDatum
+        --       outGameCfg = gdGame outputDatum in
+        --     case ( tSecondPlayer inGameCfg ) of
+        --       Nothing -> if (( tSecondPlayer inGameCfg ) /= (tSecondPlayer outGameCfg)) then
+        --                  (( lovelaces $ txOutValue ownInput) == gStake) && 
+        --                  ((lovelaces $ txOutValue ownInput) >= minGameStake)
+        --                  else
+        --                    (( lovelaces $ txOutValue ownInput) == gStake) && 
+        --                    (( lovelaces $ txOutValue ownOutput) == 2*gStake) &&
+        --                    ((lovelaces $ txOutValue ownInput) >= minGameStake)
+        --       Just x -> True 
+                       
+        info :: TxInfo    
+        info = scriptContextTxInfo ctx
+
+
+        ownInput :: TxOut
+        ownInput = case (findOwnInput ctx) of
+                    Nothing -> traceError "game input is missing"
+                    Just x -> txInInfoResolved x
+
+        -- checkMinStake :: Bool
+        -- checkMinStake 
+
+        -----------------------------------------
+        -- TODO: In the below code check if 
+        -- the `findDatum h info` is extracting 
+        -- the datum from the output.
+        -----------------------------------------
+
+        ownOutput   :: TxOut
+        outputDatum :: GameDatum
+        (ownOutput, outputDatum) = case getContinuingOutputs ctx of
+            [o] -> case txOutDatumHash o of
+                Nothing   -> traceError "wrong output type"
+                Just h -> case findDatum h info of
+                    Nothing        -> traceError "datum not found"
+                    Just (Datum d) ->  case PlutusTx.fromBuiltinData d of
+                        Just ad' -> (o, ad')
+                        Nothing  -> traceError "error decoding data"
+            _   -> traceError "expected exactly one continuing output"
 
 -- | Datum and redeemer parameter types
 data TicTacToe
@@ -176,6 +240,10 @@ validator = Scripts.validatorScript ticTacToeInstance
 gameAddress :: Address
 gameAddress = Ledger.scriptAddress (Scripts.validatorScript ticTacToeInstance)
 
+gameHash :: Ledger.ValidatorHash
+gameHash = Scripts.validatorHash ticTacToeInstance
+
+
 -- Start Game Params
 data StartParams = StartParams {
     gFirstPlayerPkh      :: !PaymentPubKeyHash
@@ -184,13 +252,29 @@ data StartParams = StartParams {
    ,gSecondPlayerMarker  :: !(Maybe BuiltinByteString)
    ,gStake               :: !Integer
    ,gDeadline            :: !POSIXTime
-   ,gCurrency             :: !CurrencySymbol
+   ,gCurrency            :: !CurrencySymbol
    ,gTokenName           :: !TokenName
    ,gState               :: !GameState
+   ,gMinGameStake        :: !Integer 
+} deriving (Generic, ToJSON, FromJSON)
+
+-- Move params - Redeemer
+data MoveParams = MoveParams {
+    mFirstPlayerPkh      :: !PaymentPubKeyHash
+   ,mFirstPlayerMarker   :: !BuiltinByteString
+   ,mSecondPlayerPkh     :: !(Maybe PaymentPubKeyHash)
+   ,mSecondPlayerMarker  :: !(Maybe BuiltinByteString)
+   ,mStake               :: !Integer
+   ,mCurrency            :: !CurrencySymbol
+   ,mTokenName           :: !TokenName
+   ,mState               :: !GameState
+   ,mMinGameStake        :: !Integer 
+   ,mChoice              :: !GameRedeemer
 } deriving (Generic, ToJSON, FromJSON)
 
 -- | The schema of the contract, with one endpoint to publish the problem with a bounty and another to sbumit solutions
 type TicTacToeSchema = Endpoint  "start" StartParams
+                       .\/ Endpoint "move" MoveParams
                        .\/ Endpoint "test" ()
 
 start :: AsContractError e => StartParams -> Contract w s e ()
@@ -201,6 +285,7 @@ start StartParams{..} = do
               {
                 tFirstPlayer = (gFirstPlayerMarker, gFirstPlayerPkh)
                ,tSecondPlayer = Nothing
+               ,tMinGameStake = gMinGameStake
                ,tGameStake = gStake
                ,tDeadline = gDeadline
                ,identifierToken = AssetClass (gCurrency, gTokenName)
@@ -210,28 +295,74 @@ start StartParams{..} = do
                 gdGame = g
                ,gdGameState = gState
             }
-        v = (Value.singleton gCurrency gTokenName 1) <> (Ada.lovelaceValueOf minLovelace )
+        -- v = (Value.singleton gCurrency gTokenName 1) <> (Ada.lovelaceValueOf minLovelace ) <> (Ada.lovelaceValueOf gStake )
+        v = (Value.singleton gCurrency gTokenName 1) <> (Ada.lovelaceValueOf gStake )        
         -- v = (Value.assetClassValue (Value.AssetClass (gCurrency, gTokenName)) 1) <> Ada.lovelaceValueOf minLovelace
         tx = Constraints.mustPayToTheScript d v
     ledgerTx <- submitTxConstraints ticTacToeInstance tx
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
     logInfo @Haskell.String $ printf "Started game with paramters %s and token %s" (Haskell.show d) (Haskell.show v)
 
+
+-- mkMove :: AsContractError e => MoveParams -> Contract w s e ()
+mkMove :: MoveParams -> Contract w s Text ()
+mkMove MoveParams{..} = do
+    logInfo @Haskell.String "-------------- Make a Move ---------------"
+    (oref, o, d@GameDatum{..}) <- findGameDatum mCurrency mTokenName
+    logInfo @Haskell.String $ printf "Datum from findDatum %s" (Haskell.show $ d)
+
+    let d' = d 
+        r = Redeemer $ PlutusTx.toBuiltinData $ Play X
+        v = (Value.singleton mCurrency mTokenName 1) <> (Ada.lovelaceValueOf mStake )
+        -- explained here https://cardano.stackexchange.com/questions/2296/lecture-6-it-2-core-hs-explaining-lookups-use-of-both-typedvalidatorlook
+        lookups = Constraints.typedValidatorLookups ticTacToeInstance Haskell.<> -- used for the output utxo with the new contract instance
+                  Constraints.otherScript validator                   Haskell.<> -- used for consuming the input contract instance
+                  Constraints.unspentOutputs (Map.singleton oref o)
+        tx = Constraints.mustPayToTheScript d' v                            <>
+             Constraints.mustValidateIn (to $ tDeadline gdGame)          <>
+             Constraints.mustSpendScriptOutput oref r
+    ledgerTx <- submitTxConstraintsWith lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+
+
+findGameDatum :: CurrencySymbol -> TokenName -> Contract w s Text (TxOutRef, ChainIndexTxOut, GameDatum)
+findGameDatum cs tn = do
+    utxos <- utxosAt $ scriptHashAddress gameHash
+    let xs = [ (oref, o)
+             | (oref, o) <- Map.toList utxos
+             , Value.valueOf (_ciTxOutValue o) cs tn == 1
+             ]
+    case xs of
+        [(oref, o)] -> case _ciTxOutDatum o of
+            Left _          -> throwError "datum missing"
+            Right (Datum e) -> case PlutusTx.fromBuiltinData e of
+                Nothing -> throwError "datum has wrong type"
+                Just d@GameDatum{..}
+                    | AssetClass(cs, tn) == (identifierToken gdGame) -> return (oref, o, d)
+                    | otherwise                                           -> throwError "game token missmatch"
+        _           -> throwError "game utxo not found"
+
 test :: forall w s e. AsContractError e => Contract w s e ()
 test = do
     logInfo @Haskell.String "-----------Printing test logs---------"
+    logInfo @Haskell.String  $ "Game Address : " <> (Haskell.show gameAddress)
+
     unspentOutputs <- utxosAt gameAddress
     -- 'collectFromScript' is a function of the wallet API. It creates a tx consuming
     -- all unspent transaction outputs at a script address and pays them to a
     -- public key address owned by this wallet.
     logInfo @Haskell.String $ Haskell.show $ Map.toList unspentOutputs
 
+    -- utxos at the gameHash
+    utxos <- utxosAt $ scriptHashAddress gameHash -- equivalent to "unspentOutputs <- utxosAt gameAddress"
+    logInfo @Haskell.String $  "Utxos at Game Hash : " <> (Haskell.show $ Map.toList utxos)
 
 -- | TicTacToe endpoints.
-endpoints :: forall  e. AsContractError e => Contract () TicTacToeSchema e ()
-endpoints = awaitPromise (start' `select` test') >> endpoints
+endpoints :: Contract () TicTacToeSchema Text ()
+endpoints = awaitPromise (start' `select` move' `select` test') >> endpoints
   where
     start' = endpoint @"start" start
+    move' = endpoint @"move" mkMove
     test' = endpoint @"test" $ const test
 
 -- mkSchemaDefinitions ''TicTacToeSchema
@@ -244,8 +375,10 @@ emulatorConfig =
     def
     def
       where
+        -- Initialize the wallet with 100Ada and the NFT token
+        -- In real-world you need to mint a NFT and assign it to the player 1
         v1 :: Value
-        v1 = Ada.lovelaceValueOf 100_000_000 <> assetClassValue token 1 -- Initialize the wallet with 100Ada and the NFT token
+        v1 = Ada.lovelaceValueOf 100_000_000 <> assetClassValue token 1 
         v2 :: Value
         v2 = Ada.lovelaceValueOf 100_000_000
 
@@ -265,8 +398,8 @@ traceConfig =
 -- test consuming the bounty when the deadline has not been reached
 myTraceTest :: Trace.EmulatorTrace ()
 myTraceTest = do
-    h1 <- Trace.activateContractWallet (knownWallet 1) $ endpoints @ContractError
-    h2 <- Trace.activateContractWallet (knownWallet 2) $ endpoints @ContractError
+    h1 <- Trace.activateContractWallet (knownWallet 1) $ endpoints 
+    h2 <- Trace.activateContractWallet (knownWallet 2) $ endpoints 
     Trace.callEndpoint @"test" h1 ()
     -- s <- Trace.waitNSlots 1
     void $ Trace.waitNSlots 1
@@ -277,6 +410,7 @@ myTraceTest = do
        ,gFirstPlayerMarker = (BuiltinByteString Haskell.. C.pack) "X"
        ,gSecondPlayerPkh = Nothing -- mockWalletPaymentPubKeyHash (knownWallet 2)
        ,gSecondPlayerMarker = Nothing -- (BuiltinByteString Haskell.. C.pack) "O"
+       ,gMinGameStake = minGameStake
        ,gStake = 10_000_000
        ,gDeadline = slotToBeginPOSIXTime def 10
        ,gCurrency = currSymbol
@@ -287,12 +421,32 @@ myTraceTest = do
     s <- Trace.waitNSlots 1
     Extras.logInfo $ "reached " ++ Haskell.show s
 
+    Trace.callEndpoint @"test" h1 ()
+    void $ Trace.waitNSlots 1
+
+    Trace.callEndpoint @"move" h1 $ MoveParams
+      {
+         mFirstPlayerPkh      = mockWalletPaymentPubKeyHash (knownWallet 1)
+        ,mFirstPlayerMarker   = (BuiltinByteString Haskell.. C.pack) "X"
+        ,mSecondPlayerPkh     = Nothing
+        ,mSecondPlayerMarker  = Nothing
+        ,mStake               = 10_000_000
+        ,mCurrency            = currSymbol
+        ,mTokenName           = tName
+        ,mState               = initGameState
+        ,mMinGameStake        = minGameStake 
+        ,mChoice              = Play O
+      }
+    void $ Trace.waitNSlots 1
 
 currSymbol :: CurrencySymbol
 currSymbol = currencySymbol "dcddcaa"
 
 tName :: TokenName
 tName = tokenName "T1"
+
+tName2 :: TokenName
+tName2 = tokenName "T2"
 
 -- Create a token of AssetClass to send it to the wallet
 token :: AssetClass
